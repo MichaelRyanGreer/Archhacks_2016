fbbbf621d0f4327f667f69b7c4143b252e052eb3
Thomas <t.m.kelly@wustl.edu>
2016-11-05 19:50:00 -0500

    Okay screw Angular

--- website/routes.py
+++ website/routes.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, send_from_directory
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+#@app.route('/static/js/<path:path>')
+#def server_static(path):
+#	return send_from_directory('static/js', path)
 
 if __name__ == '__main__':
 	app.run()
